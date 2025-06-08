40d8da4960b1456eeb92ea1046384fdfd93f399f
The-Dark-Knight <solomonnderitu99@gmail.com>
2025-06-08 14:37:00 +0300

    Calculates Cubes

--- Trial2.py
+++ Trial2.py
@@ -1,7 +1,8 @@
 x = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0]
-y = []
+y = []; z = []
 
 for xval in x:
     y.append(xval**2)
+    z.append(xval**3)
 
 print("The values squared from for loop are:{}".format(y))
